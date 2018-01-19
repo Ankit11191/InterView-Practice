a596e2e717b67c443b2d50d5dc33929a6ba132f4
Ankit mittal <mittal.ankit91@gmail.com>
2018-01-19 19:49:18 +0530

    Update Test.java

--- Webservices/WebServices1/Test.java
+++ Webservices/WebServices1/Test.java
@@ -10,12 +10,13 @@ public class Test extends Logging{
 	public static void main(String[] args) {
 
 		ArrayList<ArrayList<String>> UNPWD = ReadFromExcelFile.cellval("FileName");
-
+		
 		Login.launcg();
-		for(ArrayList<String> aaa:UNPWD)
+		for(ArrayList<String> aaa:UNPWD.subList(0, 2))
 		{
 			String[] arr=aaa.toArray(new String[aaa.size()]);
 			Login.logging(arr[0], arr[1]);
+			ReadFromExcelFile.updateCell("FileName",arr[0]);			
 		}
 		Login.logoff();
 	}
