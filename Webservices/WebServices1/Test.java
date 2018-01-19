040b78c800d8a8e3926cfcdd75dd68e3666aca06
Ankit mittal <mittal.ankit91@gmail.com>
2018-01-19 17:08:20 +0530

    main class

---
+++ Webservices/WebServices1/Test.java
@@ -0,0 +1,22 @@
+package testCases;
+
+import java.util.ArrayList;
+
+import base.Logging;
+import base.ReadFromExcelFile;
+
+public class Test extends Logging{
+
+	public static void main(String[] args) {
+
+		ArrayList<ArrayList<String>> UNPWD = ReadFromExcelFile.cellval("FileName");
+
+		Login.launcg();
+		for(ArrayList<String> aaa:UNPWD)
+		{
+			String[] arr=aaa.toArray(new String[aaa.size()]);
+			Login.logging(arr[0], arr[1]);
+		}
+		Login.logoff();
+	}
+}
